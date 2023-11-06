035bfd5990d56354b08d1bfe59438c2571a47cd6
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-11-06 20:38:48 +0300

    requre authentication path

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,7 +2,7 @@
 """ Authentication class"""
 from flask import request
 from typing import List, TypeVar
-
+import re
 
 class Auth:
     """ Authentication class"""
@@ -10,7 +10,19 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determines if authentication is required for a given path.
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """Retrieves the authorization header from the request.
