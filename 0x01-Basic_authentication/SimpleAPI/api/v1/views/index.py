9342b97b42d5798882136ed697d7c065a816d7cb
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-11-06 18:43:24 +0300

    basic authentication

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,21 +24,3 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
-@app_views.route('/unauthorized', strict_slashes=False)
-def unauthorized() -> str:
-    """GET /api/v1/unauthorized
-    Return:
-    - Unauthozed
-    """
-    abort(401)
-
-
-@app_views.route('/forbidden', strict_slashes=False)
-def forbidden() -> str:
-    """GET /api/v1/forbidden
-    Return:
-    - forbides the use of that route
-    """
-    abort(403)
