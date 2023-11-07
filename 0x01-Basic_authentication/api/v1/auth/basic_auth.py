99bd120b77368ef6d0fcb8b841208192109ddc0a
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-11-07 14:11:14 +0300

    Base64 extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 """Basic Authentication
 """
+import re
+from .auth import Auth
 
 
 class BasicAuth(Auth):
     """ Basic Authentication class"""
-    pass
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
