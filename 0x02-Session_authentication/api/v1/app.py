bd596731131a47d5ac5cf30888d2f10704f584e0
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-11-08 18:16:02 +0300

    add a new endpoint: GET /users/me to retrieve the authenticated User object.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,8 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,6 +22,8 @@ if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+if auth_type == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
@@ -60,7 +64,7 @@ def authenticate_user():
                 abort(401)
             if user is None:
                 abort(403)
-            #authenticated user to request.current_user
+            """authenticated user to request.current_user"""
             request.current_user = user
 
 
