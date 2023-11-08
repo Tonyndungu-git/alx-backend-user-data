5065f0d8e2a7d0361008280b0e4f547f2be232ff
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-11-08 18:20:33 +0300

    empty session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -6,12 +6,4 @@ from uuid import uuid4
 
 class SessionAuth:
     """  session authentication """
-    user_id_by_session_id = {}
-
-    def create_session(self, user_id: str = None) -> str:
-        """Creates a session id for the user.
-        """
-        if type(user_id) is str:
-            session_id = str(uuid4())
-            self.user_id_by_session_id[session_id] = user_id
-            return session_id
+    pass
