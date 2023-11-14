ebd7f404954493ca79a188da0145aa98bef56f71
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-11-14 12:57:27 +0300

    db model

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """" adds a new user"""
+        """Adds a new user to the database.
+        """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
